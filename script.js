dca6fbc15d9f4e084a4bc766f392f919b7c920ac
Geiselle Holt <geiselleholt@hotmail.com>
2025-03-25 23:09:01 -0600

    Working but not looping

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
-alert('Shall we play a game?')
+alert("Shall we play a game?");
 
-alert("Read my mind! \n I'll give you one word and you try to guess what word I'm thinking of. \n You have 3 guesses and 3 words. \n Submit answers in all lowercase.\n Ready?")
+alert(
+  "Read my mind! \n I'll give you a clue and you try to guess what word I'm thinking of. \n You have 3 guesses and 3 clues. \n Submit answers in all lowercase.\n Ready?"
+);
 
-alert('Rose')
-
-
-for (let i = 0; i < 3; i++) {
-    let i = prompt("What's your guess?");
-    if (i == 'titanic') {
-        alert('You read my mind!!!');
-        break;
+alert("Rose");
+let guess1 = prompt("What's your guess?");
+if (guess1 == "titanic") {
+  alert("You read my mind!!!");
+} else {
+  let guess2 = prompt(`Not even close. Here's another clue: Colossal`);
+  if (guess2 == "titanic") {
+    alert("You read my mind!!!");
+  } else {
+    let guess3 = prompt(`Close but no cigar. This is your last clue: Iceburg`);
+    if (guess3 == "titanic") {
+      alert("You read my mind!!!");
     } else {
-        i+=1
-        let i = prompt(`Not even close. Here's another word: Colossal`)
+      alert("Loser! Another Loser!");
     }
+  }
 }
